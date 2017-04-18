17aa80f99d71e53d32d3dc7a0d1dc95a4188d3a8
huangkuncan <673391138@qq.com>
2017-04-18 16:06:05 +0800

    gslog 日志类

---
+++ iPeerCloud/app/src/main/java/com/ipeercloud/com/utils/GsLog.java
@@ -0,0 +1,27 @@
+package com.ipeercloud.com.utils;
+
+import android.util.Log;
+
+/**
+ * @author 673391138@qq.com
+ * @since 17/4/18
+ * 主要功能: 日志类
+ */
+
+public class GsLog {
+    private static final String TAG = "GsLog";
+
+    public static void e(String s) {
+        Log.e(TAG, "e: ");
+    }
+
+    public static void d(String s) {
+        if (GsConfig.isDebug) {
+            Log.d(TAG, "d: ");
+        }
+    }
+
+    public static void i(String s) {
+        Log.i(TAG, "i: ");
+    }
+}
